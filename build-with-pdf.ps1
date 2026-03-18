74a9c4a0618b73120f640499bcf7b35eb2e503be
bikal-ict-international <bikal@ictinternational.com.au>
2026-03-19 10:04:09 +1100

    Clear gh-pages branch

--- build-with-pdf.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$scriptPath = Join-Path $PSScriptRoot "tools\generate-pdfs-all.ps1"
-if (-not (Test-Path $scriptPath)) {
-    throw "Pipeline script not found: $scriptPath"
-}
-
-& powershell -ExecutionPolicy Bypass -File $scriptPath @args
